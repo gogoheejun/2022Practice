a461ca0509002c24b1270e22284bdf6159aec91b
gogoheejun <heejjuunn@gmail.com>
2022-09-06 06:07:24 +0900

    front controller 도입

---
+++ springMVC/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,12 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//서블릿과 비슷한 모양의 컨트롤러 인터페이스 만듬
+public interface ControllerV1 {
+
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
