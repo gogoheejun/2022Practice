c9b1d69ecf3c6a8601d40b27facaee2fa902c691
gogoheejun <heejjuunn@gmail.com>
2022-09-12 16:09:20 +0900

    상품 등록,수정 기능

--- springMVC/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ springMVC/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -33,5 +32,73 @@ public class BasicItemController {
     public void init(){
         itemRepository.save(new Item("itemA", 1000,10));
         itemRepository.save(new Item("itemB", 2000,20));
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item,
+                            Model model){
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //@ModelAttribute("item")여기서 해줬기에 해당코드 필요없음
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute("item") Item item){
+        //이건 model.addAttribute("item",item)과 같음. 타입이 지금 Item인데 타입의 첫글자를 소문자로 바꿔서 키로 저장함.
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /*
+    ModelAttribute생략가능! 기본타입들은 RequestParam이 적용되지만
+    그 외 타입은 ModelAttribute이 적용됨
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+
+        //뷰 템플릿을 호출하는 대신에 상품상세화면을 이동하도록 다시 컨트롤러부터 호출
+        return "redirect:/basic/items/{itemId}";
     }
 }
